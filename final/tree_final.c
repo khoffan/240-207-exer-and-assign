4b73bc6ce0fcd04b2ae691ee1c6c422515488904
khoffan <khoffan222@gmail.com>
2022-10-13 00:55:08 +0700

    refector

--- final/tree_final.c
+++ final/tree_final.c
@@ -15,22 +15,25 @@ TREE mk_tree(int x,TREE right,TREE left);
 void insert_node(TREE *tp,int value);
 int root_value(TREE t);
 void print_inorder(TREE t);
+void print_preorder(TREE t);
 void print_leaves(TREE t);
 void free_tree(TREE *t);
 int main(){
     TREE t = NULL;
     insert_node(&t,6);
-    insert_node(&t,4);
-    insert_node(&t,8);
     insert_node(&t,3);
-    insert_node(&t,5);
     insert_node(&t,1);
+    insert_node(&t,4);
+    insert_node(&t,5);
+    insert_node(&t,8);
     insert_node(&t,7);
     insert_node(&t,9);
     
 
     printf("Enter Root: %d\n"); root_value(t);
     print_inorder(t);
+    printf("\n");
+    print_preorder(t);
     printf("\n");
     print_leaves(t);
 
@@ -63,17 +66,24 @@ void insert_node(TREE *tp, int value) {
 		printf("duplicate node\n");
 }
 int root_value(TREE t){
-    if(t == NULL){
-        return 0;
-    }
-    else{
+
+    if(t != NULL){
         return t->data;
+        root_value(t->leftptr);
+        root_value(t->rightptr);
     }
 }
 void print_inorder(TREE t){
     if(t != NULL){
         print_inorder(t->leftptr);
 		printf(" %d", t->data);
+		print_inorder(t->rightptr);
+    }
+}
+void print_preorder(TREE t){
+    if(t != NULL){
+		printf(" %d", t->data);
+        print_inorder(t->leftptr);
 		print_inorder(t->rightptr);
     }
 }
